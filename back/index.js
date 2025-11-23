ac5f3f0ecfa6d940fe85fa8341cdaea33f00a548
emmanuel ntoutoume <emmanuelnguema02@gmail.com>
2025-11-23 20:12:24 +0100

    Structure du Projet

---
+++ back/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// Middleware pour parser le JSON
+app.use(express.json());
+
+// Route de base
+app.get('/', (req, res) => {
+    res.send('Bienvenue sur mon serveur Node.js PRO !');
+});
+
+// Démarrer le serveur
+app.listen(PORT, () => {
+    console.log(`Serveur démarré sur http://localhost:${PORT}`);
+});
